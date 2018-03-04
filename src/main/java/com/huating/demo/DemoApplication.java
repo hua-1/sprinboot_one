262a80a9ae8cd4c6198a7e45d73e3eb8ff1f8a0f
lht <lianght@visionet.com.cn>
2018-03-04 17:11:25 +0800

    redis

--- src/main/java/com/huating/demo/DemoApplication.java
+++ src/main/java/com/huating/demo/DemoApplication.java
@@ -1,9 +1,16 @@
 package com.huating.demo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@MapperScan("com.huating.demo.com.dao")
+@EnableSwagger2
+@EnableCaching
 public class DemoApplication {
 
 	public static void main(String[] args) {
